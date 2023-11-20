4542c70fea22b5a061c8f334215441c697af7fd6
Michele <149628532+mkekk24@users.noreply.github.com>
2023-11-20 07:37:20 -0500

    Add files via upload

--- books.js
+++ books.js
@@ -1,10 +1,36 @@
-function findAuthorById(authors, id) {}
-
-function findBookById(books, id) {}
-
-function partitionBooksByBorrowedStatus(books) {}
-
-function getBorrowersForBook(book, accounts) {}
+function findAuthorById(authors, id) {
+let authorObject = authors.find(author => author.id == id)
+  return authorObject;
+}
+function findBookById(books, id) {
+let bookObject = books.find(book => book.id == id)
+return bookObject;
+}
+function partitionBooksByBorrowedStatus(books) {
+const returned = books.filter((book) => book.borrows[0].returned);
+  const unreturned = books.filter ((book) => !book.borrows[0].returned);
+  return [unreturned, returned];
+}
+function getBorrowersForBook(book, accounts) {
+const { borrows } = book;
+    const renters = borrows.map(({ id, returned })=> {
+      const account = accounts.find(account => account.id === id);
+      return {
+        ...account,
+        returned,
+      };
+    });
+ 
+    return renters
+  
+      .sort((borrowA, borrowB) => {
+        const companyA = borrowA.company;
+        const companyB = borrowB.company;
+        return companyA.localeCompare(companyB);
+      })
+     
+      .slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
